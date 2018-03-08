bafcb049416a4d3e8a889e460088bb9c4e7e8cae
Aluno <Aluno>
2018-03-08 18:25:36 -0300

    banco de dados aula 3

---
+++ Banco de Dados/Pratica_1EA.sql
@@ -0,0 +1,6 @@
+create table tabaluno (idaluno integer primary key, nome varchar(20), endereco varchar(30));
+insert into tabaluno values (1, '', '');
+describe tabaluno;
+select * from tabaluno;
+delete from tabaluno;
+drop table tabaluno;
