81c258b98ccc32fe63879444c45c612ea9db8383
IGOR AMARAL CORREA <15.00588-7@maua.br>
2018-05-15 18:11:54 -0300

    até exemplo7

--- Linguagens de Programação ll/Tiagão/exemplo5/exemplo5.js
+++ Linguagens de Programação ll/Tiagão/exemplo5/exemplo5.js
@@ -2,6 +2,9 @@ function log(a){
     a();
 }
 
-log(function() {
+log(function() { //passando uma função anônima como parmâmetro de uma função, e na função log(), o paraêmtro é executado
     console.log('hi');
 })
+
+//window.onclick = function(){ console.log("É isso")}
+//da para usar isso ^ no console do google chrome
---
+++ Linguagens de Programação ll/Tiagão/exemplo6/exemplo6.js
@@ -0,0 +1,12 @@
+/*
+function desaparece(xesque) {
+    console.log("DEU CERTO");
+}
+
+document.getElementById("xesque").addEventListener('click', desaparece);
+//document.querySelector("#xesque").addEventListener('click', desaparece); da na mesma que ade cima
+*/
+
+$("#xesque").click(function(){
+    console.log("DEU CERTO");
+});
