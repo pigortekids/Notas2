748a08f9b40dcdd710cf7f8020dfd5639075c1ca
Aluno <Aluno>
2018-10-09 18:24:58 -0300

    API top

---
+++ Linguagens de Programação ll/2semestre/4bi/projeto/public/index.php
@@ -0,0 +1,26 @@
+<?php
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+require '../vendor/autoload.php';
+
+$app = new \Slim\App;
+
+$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
+    $name = $args['name'];
+    $response->getBody()->write("Hello, $name");
+
+    return $response;
+});
+
+$app->post('/livro', function (Request $request, Response $response, array $args) {
+    
+    $batata = $request->getParsedBody();
+
+    
+
+    $respostaJSON = $response->withJson($batata, 200);
+    return $respostaJSON;
+});
+
+$app->run();
